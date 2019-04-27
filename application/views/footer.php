9906979588c390cb5bd6b577dbb9097bb36e015f
maxcxam <maxcxam1979@gmail.com>
2019-04-27 09:14:50 +0300

    gitignore

---
+++ application/views/footer.php
@@ -0,0 +1,15 @@
+</div>
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
