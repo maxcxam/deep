25b315b02643ec8d24e1eb1dfcd26a36fd723a60
maxcxam <maxcxam1979@gmail.com>
2019-04-27 19:27:59 +0300

    gitignore

---
+++ application/models/Main_model.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Main_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function all_posts()
+    {
+        
+    }
+}
